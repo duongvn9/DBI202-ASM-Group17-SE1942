af201426b0ce2fa994c5f3d5636621ee027abc44
Monomee <hoangthth12@gmail.com>
2025-03-04 23:08:32 +0700

    create hotel_booking database

--- Hotel_Booking.sql
+++ Hotel_Booking.sql
@@ -1,1 +1,8 @@
-use Hotel_Booking
+USE [master]
+IF EXISTS (SELECT name FROM master.sys.databases WHERE name = N'Hotel_Booking')
+drop database [Hotel_Booking]
+GO
+/****** Object:  Database [Hotel_Booking]    Script Date: 04/03/2025 23:07:00 PM ******/
+CREATE DATABASE [Hotel_Booking]
+GO
+USE [FUH_COMPANY2]
