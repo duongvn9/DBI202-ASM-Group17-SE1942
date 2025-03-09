e235314b9fead8f0f7c44d2ca1011a775e981aa4
Monomee <hoangthth12@gmail.com>
2025-03-09 20:57:17 +0700

    some sql test

--- Main.sql
+++ Main.sql
@@ -1,7 +1,25 @@
-SELECT * FROM CUSTOMER
+﻿SELECT * FROM CUSTOMER
 SELECT * FROM ROOM
 SELECT * FROM SERVICE
 SELECT * FROM BOOKING
 SELECT * FROM PAYMENT
 SELECT * FROM STAFF
 SELECT * FROM ROOM_SERVICE
+
+--Kiểm tra phòng trống
+SELECT r.RoomID, r.RoomType, r.RoomPrice, r.RoomStatus
+FROM ROOM r
+WHERE r.RoomStatus = 'Available'
+AND NOT EXISTS (
+    SELECT 1 
+    FROM BOOKING b 
+    WHERE b.RoomID = r.RoomID 
+    AND b.BookingStatus IN ('Confirmed', 'Checked-in')
+    AND (
+        (b.CheckInDate <= '2025-03-15' AND b.CheckOutDate >= '2025-03-10')
+    )
+)
+--Deluxe
+SELECT *
+FROM ROOM
+WHERE RoomType = 'Deluxe'
